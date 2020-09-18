6c37022bc8a63dfe976387c024b47ed41a0089c8
Tais Guissi <taisguissi_1@hotmail.com>
2020-09-18 14:05:18 -0300

    Cabeçalho Mobile

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $email = 'developertgs@gmail.com';
 $telefone = '(11) 47484626';
 $whatsapp = '(11) 966273756';
 $whatsapp_link = '5511966273756';
-$nome_loja = 'Loja Guissi';
+$nome_loja = 'Loja SPFC';
 
 ?>
